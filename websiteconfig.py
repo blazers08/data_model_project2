cc1f01164c43bbd9a7ecc466ff9e3e897a064ef5
Denny <Denny>
2017-11-24 21:50:56 +0800

    修正不小心被改的DB Port

--- websiteconfig.py
+++ websiteconfig.py
@@ -5,7 +5,7 @@ TEMPLATES_AUTO_RELOAD = True  # html debug mode
 
 # flask-mysql setting
 MYSQL_DATABASE_HOST = 'localhost'  # default
-MYSQL_DATABASE_PORT = 3310  # default
+MYSQL_DATABASE_PORT = 3306  # default
 MYSQL_DATABASE_CHARSET = 'utf8'  # default
 MYSQL_DATABASE_USER = '106adb'
 MYSQL_DATABASE_PASSWORD = '106adb'
